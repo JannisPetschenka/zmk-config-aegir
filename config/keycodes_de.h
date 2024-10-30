ff18128e2e3008fee30f6e954d045f17029ca12b
JannisPetschenka <jannis.petschenka@gmx.de>
2024-10-30 17:21:26 +0100

    Add basic zmk config

---
+++ config/keycodes_de.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#define DE_AT LA(L)         // @
+#define DE_EURO LA(E)       // €
+#define DE_ACUT EQUAL       // ´
+#define DE_LPRN LS(N8)      // (
+#define DE_RPRN LS(N9)      // )
+#define DE_LCBR LA(N8)      // {
+#define DE_RCBR LA(N9)      // }
+#define DE_LBRC LA(N5)      // [
+#define DE_RBRC LA(N6)      // ]
+#define DE_HASH BSLH        // #
+#define DE_DQUO LS(N2)      // "
+#define DE_QUOT LS(BSLH)    // '
+#define DE_PIPE LA(N7)      // |
+#define DE_CARET GRAVE      // ^
+#define DE_SCLN LS(COMMA)   // ;
+#define DE_COLN LS(DOT)     // :
+#define DE_BSLS LS(LA(N7))  // (backslash)
+#define DE_SLSH LS(N7)      // /
+#define DE_RABK LS(GRAVE)   // >
+#define DE_LABK GRAVE       // <
+#define DE_MINS SLASH       // -
+#define DE_UNDS LS(SLASH)   // _
+#define DE_ASTR LS(RBKT)    // *
+#define DE_PLUS RBKT        // +
+#define DE_EQL LS(N0)       // =
+#define DE_TILDE LS(RA(N8)) // ~
+
+// linux
+
+#define LN_DE_LBRC RA(N5)   // [
+#define LN_DE_RBRC RA(N6)   // ]
+#define LN_DE_LPRN LS(N8)   // (
+#define LN_DE_RPRN LS(N9)   // )
+#define LN_DE_LCBR RA(N8)   // {
+#define LN_DE_RCBR RA(N9)   // }
+#define LN_TILDE LS(RA(N8)) // ~
+#define LN_DE_LABK NUBS     // <
+#define LN_DE_RABK LS(NUBS) // >
